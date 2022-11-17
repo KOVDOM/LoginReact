a1676c99060b5d8422a24048b4e20d323f12ab46
KOVDOM <kovacsdominik63@gmail.com>
2022-11-17 18:01:27 +0100

    kész

--- app.js
+++ app.js
@@ -1,78 +1,114 @@
-ReactDOM.render(React.createElement(App), document.getElementById("app-container"));
-
 function App(){
-    const [isActive, setActive]=React.useState(false);
     return React.createElement(
-        "div",
+        "form",
         {
-            className: 'border'
+            className: "border p-5",
         },
-        'Login app',
-        React.createElement(BoxComponent, {isActive: isActive}),
-        React.createElement(ButtonComponent, {isActive: isActive, setActive: setActive}),
-        "App szintű state: " + (isActive ? "Aktív" : "Inaktív")
-    );
-}
-
-function ButtonComponent({isActive, setActive}){    
-    return React.createElement('div',{
-        style: {
-            width:"200px",
-            height: "200px",
+        React.createElement(LoginInputComponent, {}),
+        React.createElement(PasswordInputComponent, {}),
+        React.createElement(ButtonComponent, {})
+      );
+    }
+    function LoginInputComponent(){
+      return React.createElement(
+        "div", {
+          className:"mb-3 row"
         },
-        className: "p-2 m-5 border rounded",
-    },
-    React.createElement(
-        "input",
-        {
-            type: "text",
-            id: "user",
-            className: "Login",
+        React.createElement(
+          "label", {
+            for:"login",
+            className:"form-label col-2"
+          },
+          "Login: "
+        ),
+        React.createElement(
+          "div", {
+            className: "col-10"
+          }, 
+          React.createElement(
+          "input", {
+            type:"text", 
+            id:"login",
+            className:"form-control"
+          }
+        )
+        )
+      )
+    }
+    function PasswordInputComponent(){
+      return React.createElement(
+        "div", {
+          className:"mb-3 row"
         },
-    ),
-    React.createElement(
-        "input",
-        {
-            type: "password",
-            id: "passW",
-            className: "Password",
+        React.createElement(
+          "label", {
+            for:"pwd",
+            className:"form-label col-2"
+          },
+          "Pwd: "
+        ),
+        React.createElement(
+          "div", {
+            className: "col-10"
+          }, 
+          React.createElement(
+          "input", {
+            type:"password", 
+            id:"pwd",
+            className:"form-control"
+          }
+        )
+        )
+      )
+    }
+    
+    function ButtonComponent() {
+      return React.createElement(
+        "div", {
+          className:"mb-3 row"
         },
-    ),
-    React.createElement(
-        "button",
-        {
-            className: "btn bg-primary m-2",
-            onClick: ()=>{                
-                const user=document.getElementById('user').value;
-                const passW=document.getElementById('passW').value;
-                console.log(user);
-                console.log(passW);
-                if(user=="abc" && passW=="123")
-                {
-                    setActive(true);
-                    alert("Sikeres bejelnetkezés!");
-                }                
-                else if(user!="abc" || passW!="123")
-                {
-                    setActive(false);
-                    alert("Sikertelen bejelentkezés");
+        React.createElement(
+          "div", {
+            className:"col-2"
+          }
+        ),
+        React.createElement(
+          "div", {
+            className: "col-10"
+          }, 
+          React.createElement(
+            "button",
+            {
+              className: "btn btn-primary font-weight-bold text-uppercase",
+              type: "button",
+              onClick: () => {
+                const logininput = document.getElementById("login");
+                const pwdinput = document.getElementById("pwd");
+                const loginval = logininput.value;
+                const pwdval = pwdinput.value;
+                if(loginval === "abc"){
+                  logininput.style.backgroundColor = "#baf7ba";
+                } else if(loginval === "") {
+                  logininput.style.backgroundColor = "unset";
+                } else {
+                  logininput.style.backgroundColor = "#f19c9c";
                 }
-            }
-        },
-        "Küldés"
-    ),
-    );
-}
+                if(pwdval === "123"){
+                  pwdinput.style.backgroundColor = "#baf7ba";
+                }else if(pwdval === "") {
+                  pwdinput.style.backgroundColor = "unset";
+                } else {
+                  pwdinput.style.backgroundColor = "#f19c9c";
+                }
+              }
+            },
+            "Küldés"
+          )
+        )
+      )
+    }  
 
-function BoxComponent(props){    
-    return React.createElement("div",
-    {
-        style: {
-            width:"200px",
-            height: "200px",
-            backgroundColor: props.isActive ? "green" : "firebrick"
-        },
-        className: "p-2 m-5 rounded",
-    },
-    props.isActive ? "Aktív" : "Inaktív");
-}
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
